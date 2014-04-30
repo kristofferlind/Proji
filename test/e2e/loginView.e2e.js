19153bd45a24568c7d15f01c53cf07fb20d808a2
kl222jy <kl222jy@student.lnu.se>
2014-04-30 17:55:55 +0200

    Sprint 3, dag 3
    S1E2E3 klar, ingen push igår eftersom hela dagen försvann och inte
    mycket som hänt idag eftersom det blev halvdag.

--- test/e2e/loginView.e2e.js
+++ test/e2e/loginView.e2e.js
@@ -133,7 +133,7 @@ describe('View: /login', function() {
 
             $('button.pure-button.pure-button-primary').click();
             browser.waitForAngular();
-            browser.sleep(3000);
+            browser.sleep(5000);
             expect($('.content h1').getText()).toEqual('Overview');
         });
     });
