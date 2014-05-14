d4e179e90c3f13f78c546444730553ade3eca588
kl222jy <kl222jy@student.lnu.se>
2014-05-14 12:10:47 +0200

    travis, integrationstester borde misslyckas
    föregående build borde fått status failing

--- Gruntfile.js
+++ Gruntfile.js
@@ -366,7 +366,7 @@ module.exports = function(grunt) {
         protractor: {
             options: {
                 configFile: 'protractor.conf.js', // Default config file
-                keepAlive: true, // If false, the grunt process stops when the test fails.
+                keepAlive: false, // If false, the grunt process stops when the test fails.
                 noColor: false, // If true, protractor will not use colors in its output.
                 args: {
                     seleniumServerJar: 'node_modules/protractor/selenium/selenium-server-standalone-2.41.0.jar',
